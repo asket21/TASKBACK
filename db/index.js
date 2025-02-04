3b52e2c2004360fc534f6b41bd3f19ee8889829b
Asket21 <g.i.kr@yandex.ru>
2025-02-04 21:13:26 +0300

    Вся разработка тут, продакшн в Мейн

--- db/index.js
+++ db/index.js
@@ -1,19 +1,21 @@
-const {Pool} = require('pg');
+const {Sequelize} = require('sequelize');
 
 
 require('dotenv').config();
 
 
-const pool = new Pool(
+const sequelize = new Sequelize(
     {
-        user: process.env.DB_USER,
-        host:process.env.DB_HOST,
+
         database:process.env.DB_NAME,
-        password:process.env.DB_PASSWORD,
+        username: process.env.DB_USER,
+        password:process.env.DB_PASSWORD, {
+        host:process.env.DB_HOST,
         port: process.env.DB_PORT
+    }
     });
 
-module.exports = pool;
+module.exports = sequelize;
 
 
 
